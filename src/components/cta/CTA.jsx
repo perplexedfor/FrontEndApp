3141583961e2632c84893242736b9783f36173e2
perplexedfor <gusainabhinav037@gmail.com>
2024-02-07 20:52:11 +0530

    commit

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './cta.css'
+function CTA() {
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        
+      </div>
+    </div>
+  )
+}
+
+export default CTA
